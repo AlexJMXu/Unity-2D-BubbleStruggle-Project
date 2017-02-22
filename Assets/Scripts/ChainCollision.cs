cf84ca7f9f40f44d76b0068ea4919b875060a7a9
Alexander Xu <ajm.xu@outlook.com>
2017-02-22 00:24:01 +0000

    Initial Commit
    Bubble Struggle Replica

---
+++ Assets/Scripts/ChainCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ChainCollision : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Chain.isFired = false;
+
+		if (col.CompareTag("Ball")) {
+			col.GetComponent<Ball>().Split();
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 4f;
+
+	public Rigidbody2D rb;
+
+	private float movement = 0f;
+
+	public float slowness = 10f;
+
+	private bool isDead = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) movement = Input.GetAxisRaw("Horizontal") * speed;
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.collider.tag == "Ball") {
+			isDead = true;
+			movement = 0f;
+			Vector2 dir = col.gameObject.GetComponent<Ball>().rb.position - rb.position;
+			rb.AddForce(10000f * dir, ForceMode2D.Force);
+			StartCoroutine(RestartLevel());
+		}
+	}
+
+	IEnumerator RestartLevel() {
+		Time.timeScale = 1f/slowness;
+		Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+
+		yield return new WaitForSeconds(1f/slowness);
+
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
